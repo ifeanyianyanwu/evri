337e3050229f9207c0e50ef8300210251a2cf5a7
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-07 03:36:08 +0100

    added more configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-one": "url('/src/assets/Home-Background-One.webp')",
+        "hero-two": "url('/src/assets/Home-Background-Two.webp')",
+        "hero-three": "url('/src/assets/Home-Background-Three.webp')",
+      },
+      // animation: {
+      //   wobble: "wobble 1.5s ease-in-out infinite",
+      // },
+      // keyframes: {
+      //   wobble: {
+      //     "0%": {
+      //       transform: "translateX(0)",
+      //     },
+      //     "100%": {
+      //       transform: "translateX(2rem)",
+      //     },
+      //   },
+      // },
+      transitionProperty: {
+        fader: "transform, opacity",
+        transform: "transform",
+      },
+    },
   },
   plugins: [],
 };
