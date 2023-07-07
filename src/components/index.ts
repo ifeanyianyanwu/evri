8f43906c169a586f4fdddffdc9774dc69d0dd48a
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-07 03:38:36 +0100

    update

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Navbar from "./navbar/Navbar";
+import Hero from "./hero/Hero";
+import Explore from "./explore/Explore";
+import Brands from "./brands/Brands";
+import Footer from "./footer/Footer";
+
+export { Navbar, Hero, Explore, Brands, Footer };
