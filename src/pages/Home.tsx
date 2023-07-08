4d48a02dfc4f13d3d565ba225699bb386262eb56
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-08 17:17:35 +0100

    added cart and moved mobile nav to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import { BestSellers, Brands, Explore, Footer, Hero } from "../components";
+import Cart from "../components/cart/Cart";
+import ToggleNavbar from "../components/navbar/ToggleNavbar";
 
 const Home = () => {
   return (
     <div>
+      <Cart />
+      <ToggleNavbar />
       <Hero />
       <Explore />
       <BestSellers />
