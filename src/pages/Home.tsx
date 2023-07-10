b933b786e8c9d07ff3b7e6fcb5c7c73e7f9c2e95
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-10 18:30:34 +0100

    implemented scroll to top

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import { BestSellers, Brands, Explore, Footer, Hero } from "../components";
 import Cart from "../components/cart/Cart";
 import ToggleNavbar from "../components/navbar/ToggleNavbar";
 import WishList from "../components/wishlist/WishList";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.slice(1));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [location]);
+
   return (
     <div>
       <Cart />
