a1e30c0d055f0f9dcac6fb5553a17b874a4a6206
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-14 03:09:08 +0100

    style update

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -4,14 +4,17 @@ type IProps = {
   children: ReactNode;
   onClick?: () => void;
   type?: "button" | "submit" | "reset" | undefined;
+  fullwidth?: boolean;
 };
 
-const Button = ({ children, onClick, type }: IProps) => {
+const Button = ({ children, onClick, type, fullwidth }: IProps) => {
   return (
     <button
       type={type || "button"}
       onClick={onClick}
-      className="border-solid border-black border px-6 py-3 hover:bg-gray-800 hover:text-white transition-all duration-300 sm:w-fit w-full"
+      className={`border-solid border-black border px-6 py-3 hover:bg-gray-800 hover:text-white transition-all duration-300 ${
+        fullwidth ? "sm:w-full" : "sm:w-fit"
+      } w-full`}
     >
       {children}
     </button>
