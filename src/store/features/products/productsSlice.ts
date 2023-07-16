0e3b817b499034a4436220e98e282da06067de5c
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-16 15:09:49 +0100

    added logic for product filters

--- src/store/features/products/productsSlice.ts
+++ src/store/features/products/productsSlice.ts
@@ -1,13 +1,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { CartItem, Product, ProductList } from "../../../types";
 
 type SliceState = {
-  filters: {};
+  filters: {
+    categories: string[];
+    colors: string[];
+    sort: string;
+    [key: string]: string[] | string;
+  };
   searchText: string;
 };
 
 const initialState: SliceState = {
-  filters: {},
+  filters: {
+    categories: [],
+    colors: [],
+    sort: "",
+  },
   searchText: "",
 };
 
@@ -16,10 +24,55 @@ const productsSlice = createSlice({
   initialState,
   reducers: {
     searchProducts: (state, action: PayloadAction<string>) => {
-      state.searchText = action.payload.toLowerCase();
+      state.searchText = action.payload;
+    },
+    updateFilters: (
+      state,
+      action: PayloadAction<{ name: string; filter: string }>
+    ) => {
+      switch (action.payload.name) {
+        case "Category":
+          const categoryFilter = state.filters.categories.find(
+            (item) => item === action.payload.filter
+          );
+          if (categoryFilter) {
+            state.filters.categories = state.filters.categories.filter(
+              (item) => item !== categoryFilter
+            );
+          } else {
+            state.filters.categories.push(action.payload.filter);
+          }
+          break;
+
+        case "Color":
+          const colorFilter = state.filters.colors.find(
+            (item) => item === action.payload.filter
+          );
+          if (colorFilter) {
+            state.filters.colors = state.filters.colors.filter(
+              (item) => item !== colorFilter
+            );
+          } else {
+            state.filters.colors.push(action.payload.filter);
+          }
+          break;
+
+        case "Sort":
+          state.filters.sort = action.payload.filter;
+          break;
+
+        default:
+          break;
+      }
+    },
+    clearFilters: (state) => {
+      state.filters.categories = [];
+      state.filters.colors = [];
+      state.filters.sort = "";
     },
   },
 });
 
-export const { searchProducts } = productsSlice.actions;
+export const { searchProducts, updateFilters, clearFilters } =
+  productsSlice.actions;
 export default productsSlice.reducer;
