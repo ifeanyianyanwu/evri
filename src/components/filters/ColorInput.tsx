3b371cfec5050d2f3348c8340bfb61bf102cbdf5
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-16 15:10:18 +0100

    created input for color selection in filters

--- src/components/filters/ColorInput.tsx
+++ src/components/filters/ColorInput.tsx
@@ -1,15 +1,30 @@
-import { useEffect, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import convert from "color-convert";
+import { useAppSelector } from "../../store/hooks";
 
-export const ColorInput = ({ item }: { item: string }) => {
+type IProps = {
+  item: string;
+  onChange: (e: SyntheticEvent) => void;
+};
+
+export const ColorInput = ({ item, onChange }: IProps) => {
   const [color, setColor] = useState("");
+  const { filters } = useAppSelector((state) => state.products);
+
   useEffect(() => {
     setColor(convert.hex.keyword(item));
   }, []);
 
   return (
     <div className="flex gap-2 items-center">
-      <input type="checkbox" id={item} />
+      <input
+        type="checkbox"
+        id={item}
+        value={item}
+        onChange={onChange}
+        name="Color"
+        checked={filters.colors.includes(item)}
+      />
       <label htmlFor={item} className="capitalize font-light">
         {color}
       </label>
