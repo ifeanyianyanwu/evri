6f5853da73855bac214a6e6a812303b1f209d8b2
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-07-19 18:23:13 +0100

    update

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Error = () => {
   return (
     <div className="inset-0 flex items-center justify-center fixed p-8">
-      <p className="text-2xl">
+      <p className="text-base">
         Something went wrong... <br /> Please try again
       </p>
     </div>
